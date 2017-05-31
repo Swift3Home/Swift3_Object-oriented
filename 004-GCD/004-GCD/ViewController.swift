721d0f94c01bc01137d59df90adf05b6a7867863
lichuanjun <lichuanjun@niwodai.net>
2017-05-31 14:27:42 +0800

    GCD增加尾随闭包的讲解

--- 004-GCD/004-GCD/ViewController.swift
+++ 004-GCD/004-GCD/ViewController.swift
@@ -14,9 +14,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        loadData { (result) in
-            print("获取的新闻数据 \(result)")
+//        loadData { (result) in
+//            print("获取的新闻数据 \(result)")
+//        }
+        
+        // 尾随闭包
+        // 如果函数的最后一个参数是闭包，函数参数可以提前结束，最后一个参数直接使用{},包装闭包的代码
+        // loadData(completion: <#T##([String]) -> ()#>)
+        loadData() { (result) in
+            print(result)
         }
+        
+        // 按照函数本身编写的结果  原始写法
+        // 关于尾随闭包
+        // - 1. 要能看懂
+        // - 2.能够慢慢的编写，大多数 Xcode 的只能提示会帮我们做
+        loadData(completion: { (result) -> () in
+            print(result)
+        })
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -24,6 +40,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func demo() -> () {
+        // 尾随闭包
+        DispatchQueue.global().async {
+            
+            //嵌套的 GCD Xcode 不会改成尾随闭包
+            DispatchQueue.main.async(execute: { 
+                
+            })
+        }
+        
+        // 尾随闭包
+        DispatchQueue.main.async { 
+            
+        }
+    }
+    
     /*
      在异步执行任务，获取结果，通过block/ 闭包 回调
      Swift 中闭包的应用场景和block完全一致
