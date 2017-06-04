46f224320f8b174b1c3580ef713797f0d9f9779a
lichuanjun <lichuanjun@niwodai.net>
2017-06-04 18:45:55 +0800

    使用 session 加载百度数据

---
+++ 013-加载百度/013-加载百度/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  013-加载百度
+//
+//  Created by lichuanjun on 2017/6/4.
+//  Copyright © 2017年 lichuanjun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // URL 的构造函数可以返回nil
+        // 构造函数就是实例化对象的
+        let url = URL(string: "http://www.baidu.com")
+        
+        // 发起网络请求
+        // - 和 OC 的区别：Swift-闭包的所有参数，需要自己写；
+        //               OC-是直接带入
+        // - 优点：如果不关心的值，可以直接`_`忽略
+        URLSession.shared.dataTask(with: url!) { (data, _, error) in
+            
+//            if(error != nil) {
+//                print("网络请求失败")
+//                return
+//            }
+            
+            guard let data = data else {
+                print("网络请求失败 \(String(describing: error))")
+                
+                return
+            }
+            
+            // 将 data 转换为 String
+            let html = String(data: data, encoding: .utf8)
+            
+            print(html ?? "")
+            
+        }.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
