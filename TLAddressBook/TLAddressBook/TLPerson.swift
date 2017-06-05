362e1b89c9a946b2b357c55398f01685b50a1739
lichuanjun <lichuanjun@niwodai.net>
2017-06-05 22:30:04 +0800

    定义模型

---
+++ TLAddressBook/TLAddressBook/TLPerson.swift
@@ -0,0 +1,17 @@
+//
+//  TLPerson.swift
+//  TLAddressBook
+//
+//  Created by lichuanjun on 2017/6/5.
+//  Copyright © 2017年 lichuanjun. All rights reserved.
+//
+
+import UIKit
+
+class TLPerson: NSObject {
+    
+    var name: String?
+    var phone: String?
+    var title: String?
+    
+}
