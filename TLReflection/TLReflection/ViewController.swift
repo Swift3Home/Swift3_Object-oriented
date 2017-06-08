41999a1fe35162fd7f2e30789d1d9834e17fc1df
lichuanjun <lichuanjun@niwodai.net>
2017-06-08 09:54:15 +0800

    用反射机制加载类和重点

---
+++ TLReflection/TLReflection/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TLReflection
+//
+//  Created by lichuanjun on 2017/6/8.
+//  Copyright © 2017年 lichuanjun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.brown
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
