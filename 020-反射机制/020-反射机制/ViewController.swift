8ad3bf5e21ce694bea7e126022e9229670617a0c
lichuanjun <lichuanjun@niwodai.net>
2017-06-08 09:13:37 +0800

    用纯代码加载启动控制器

---
+++ 020-反射机制/020-反射机制/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  020-反射机制
+//
+//  Created by lichuanjun on 2017/6/8.
+//  Copyright © 2017年 lichuanjun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.red
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
